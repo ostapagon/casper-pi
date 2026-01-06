e1463c0a3f8bf127f2524e84f82b3aba752ae818
casper <ostap.hembara@gmail.com>
2026-01-07 01:02:53 +0200

    Add default one

--- voice_assistant.py
+++ voice_assistant.py
@@ -7,7 +7,7 @@ from vosk import Model, KaldiRecognizer
 RATE = 16000
 WAKE_WORD = "casper"
 MODEL_PATH = "assets/vosk-model-small-en-us-0.15"
-CHUNK = 4000
+CHUNK = 1024
 
 def main():
     # Load model
@@ -20,23 +20,9 @@ def main():
     
     # Setup audio
     pa = pyaudio.PyAudio()
-    
-    # Find first available input device
-    input_device = None
-    for i in range(pa.get_device_count()):
-        info = pa.get_device_info_by_index(i)
-        if info['maxInputChannels'] > 0:
-            input_device = i
-            break
-    
-    if input_device is None:
-        print("Error: No input device found")
-        pa.terminate()
-        return
-    
     # Open stream
     stream = pa.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
-                     input_device_index=input_device, frames_per_buffer=CHUNK)
+                    frames_per_buffer=CHUNK)
     
     try:
         while True:
